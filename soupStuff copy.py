2f54a3542b0af88dd053fbea1fc111a4df90dbfd
villaNich <160658585+villaNich@users.noreply.github.com>
2024-02-22 08:56:07 -0700

    Working webscraping code

--- soupStuff copy.py
+++ soupStuff copy.py
@@ -1,11 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
+import requests 
+import pandas as pd
 
-url = "https://www.oddsshopper.com/tools/arbitrage/CO"
+#url = 'https://en.wikipedia.org/wiki/List_of_best-selling_video_games'
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
 page = requests.get(url)
-soup = BeautifulSoup(page.text, 'css')
-newSoup = soup.find('table',
-                    class_='MuiGrid-root MuiGrid-grid-xs-12 css-7j6nx7')
-print(newSoup)
-print("hello there")
-print("idk brah i just be cooling")
+soup = BeautifulSoup(page.text, 'html.parser')
+tab = soup.find_all('table')[1]
+##print(tab)
+titles = tab.find_all('th')
+#print(titles)
+
+all_titles = [title.text.strip() for title in titles]
+#print(all_titles)
+
+df = pd.DataFrame(columns = all_titles)
+
+column_data = tab.find_all('tr')
+for row in column_data[1:]: 
+    row_data= row.find_all('td')
+    individual_row_data = [data.text.strip() for data in row_data]
+    #print(individual_row_data)
+
+    length = len(df)
+    print(length)
+    
+    df.loc[length] = individual_row_data
+df   
